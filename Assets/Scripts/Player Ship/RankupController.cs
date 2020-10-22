b5dd72819ce5dd0c0702989ac4da9c49b14d8757
Jonathan <jonathan@carter.games>
2020-10-22 18:47:40 +0100

    Started Rankup code
    Too lousy from blood donation to focus on this today, will focus on it tomorrow though to get some progress on the rankup system and have all weapons work.

---
+++ Assets/Scripts/Player Ship/RankupController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+namespace CarterGames.CWIS
+{
+    public class RankupController : MonoBehaviour
+    {
+        [SerializeField] private Ranks[] weaponRanks;
+        [SerializeField] private int[] usesKills;
+
+
+        /// <summary>
+        /// Sets the uses/kills value for use elsewhere. 
+        /// </summary>
+        /// <param name="valueToChange">element tp change</param>
+        /// <param name="value">value to enter.</param>
+        public void SetUsesKill(int valueToChange, int value)
+        {
+            usesKills[valueToChange] = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Rankup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+namespace CarterGames.CWIS
+{
+    public enum Ranks { None, Chev1, Chev2, Chev3, Star1, Star2, Star3 }
+
+    public class Rankup : MonoBehaviour
+    {
+        private Ranks currentRank;
+
+
+        public void SetRank(Ranks value)
+        {
+            currentRank = value;
+        }
+
+
+        public Ranks GetRank()
+        {
+            return currentRank;
+        }
+    }
+}
