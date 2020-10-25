a3420eb0a559fb39f1713b26129f7f5f0349e263
Jonathan <jonathan@carter.games>
2020-10-25 09:08:50 +0000

    New Input Controls + Mobile Element Script

---
+++ Assets/Scripts/UI Scripts/MobileElement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+namespace CarterGames.CWIS.Mobile
+{
+    public class MobileElement : MonoBehaviour
+    {
+        private void Awake()
+        {
+#if UNITY_ANDROID
+            gameObject.SetActive(true);
+#else
+            gameObject.SetActive(false);
+#endif
+        }
+    }
+}
