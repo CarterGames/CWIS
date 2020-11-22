daadb7ec44e4dd6daf52cfe168f4fac9cd69aa24
Jonathan <jonathan@carter.games>
2020-11-22 22:06:00 +0000

    Endless Survival WIP

---
+++ Assets/Scripts/Endless/CrateSpawner.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using CarterGames.Utilities;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+
+namespace CarterGames.CWIS.EndlessSurvival
+{
+    public class CrateSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject crateObject;
+        [SerializeField] private float[] minMaxX;
+        [SerializeField] private float[] minMaxWait;
+        [SerializeField] private int maxCrates;
+
+        private GameObject[] cratePool;
+        private bool isCoR;
+
+
+        private void Start()
+        {
+            cratePool = new GameObject[maxCrates];
+
+            for (int i = 0; i < cratePool.Length; i++)
+            {
+                GameObject _go = Instantiate(crateObject);
+                _go.name = "*** Crate Object Pool ***";
+                _go.SetActive(false);
+                cratePool[i] = _go;
+            }
+        }
+
+
+        private void Update()
+        {
+            if (!isCoR)
+            {
+                StartCoroutine(CrateSpawnerCo());
+            }
+        }
+
+
+        private IEnumerator CrateSpawnerCo()
+        {
+            isCoR = true;
+
+            for (int i = 0; i < maxCrates; i++)
+            {
+                if (!cratePool[i].activeInHierarchy)
+                {
+                    Vector3 spawnPos = ChooseSpawnLocation();
+                    cratePool[i].transform.position = new Vector3(spawnPos.x, 4, spawnPos.z);
+                    cratePool[i].SetActive(true);
+                    break;
+                }
+            }
+
+            yield return new WaitForSeconds(GetRandom.Float(minMaxWait[0], minMaxWait[1]));
+            isCoR = false;
+        }
+
+
+        private Vector3 ChooseSpawnLocation()
+        {
+            return Random.onUnitSphere * (Random.Range(500, 2000));
+        }
+    }
+}
---
+++ Assets/Scripts/Endless/SupplyCrate.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+/*
+*  Copyright (c) Jonathan Carter
+*  E: jonathan@carter.games
+*  W: https://jonathan.carter.games/
+*/
+
+namespace CarterGames.CWIS
+{
+    public class SupplyCrate : MonoBehaviour
+    {
+        [SerializeField] private GameObject crateuiPrefab;
+        [SerializeField] private float moveSpd = 10;
+
+        private Rigidbody rB;
+        private Transform crateAnchor;
+
+        public int ammoStandard;
+        public int ammoSpecial;
+        public int missiles;
+
+
+        private void OnEnable()
+        {
+            ammoStandard = Random.Range(30, 300);
+            ammoSpecial = Random.Range(2, 10);
+            missiles = Random.Range(-5, 4);
+        }
+
+
+        private void Start()
+        {
+            rB = GetComponent<Rigidbody>();
+
+            rB.velocity = (Vector3.zero - transform.position).normalized * moveSpd;
+
+            crateAnchor = GameObject.FindGameObjectWithTag("crateanchor").transform;
+        }
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Bullet") || other.gameObject.CompareTag("Player"))
+            {
+                // splits CWIS ammo evenly between both guns
+                int ammoSplit = Mathf.Abs(ammoStandard / 2);
+
+
+                // missiles, only if it is a positive value and below the max value
+                if (missiles > 0)
+                {
+                    //ship.shipMissiles += missiles;
+                }
+
+
+                // make the ui showing what was gotten
+                GameObject _go = Instantiate(crateuiPrefab, crateAnchor);
+
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
